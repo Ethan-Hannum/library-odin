45fa313940fbdb75e6d48faa05c6025dee2cf906
Ethan Hannum <ethanhannum10@gmail.com>
2026-03-18 23:05:12 -0700

    Make card display rest of information
    The forEach method in the displayCard function was fleshed out to
    display the author, pages of the book and if the book was read.
    Also added some more test books.

--- scripts.js
+++ scripts.js
@@ -14,7 +14,9 @@ function addBookToLibrary(int) {
 }
 
 let hungerGames = new Book("Suzanne Collins", "The Hunger Games", "500", "r");
-let testBook = new Book("Cool Author", "Cool Book", "1000", "nr")
+let testBook = new Book("Cool Author", "Cool Book", "1000", "nr");
+let mazeRunner = new Book("James Dashner", "The Maze Runner", "416", "r");
+let artOfWar = new Book("Sun Tzu", "The Art of War", "200", "nr");
 
 addBookToLibrary(hungerGames);
 addBookToLibrary(testBook);
@@ -28,6 +30,18 @@ function displayBooks() {
         const cardTitle = document.createElement("h2");
         cardTitle.textContent = `${book.title}`;
         card.appendChild(cardTitle);
+
+        const cardAuthor = document.createElement("h3");
+        cardAuthor.textContent = `${book.author}`;
+        card.appendChild(cardAuthor);
+
+        const cardPages = document.createElement("p");
+        cardPages.textContent = `${book.pages}`;
+        card.appendChild(cardPages);
+
+        const cardStatus = document.createElement("p");
+        cardStatus.textContent = `${book.status}`;
+        card.appendChild(cardStatus);
     }, this)
 }
 
